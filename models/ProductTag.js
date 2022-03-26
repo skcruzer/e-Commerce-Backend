9fca162f43b4e35e28ca96cf18b05a04ee8ab3b2
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-03-25 17:24:30 -0700

    product tag reqs

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,32 @@
 const { Model, DataTypes } = require('sequelize')
 
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection.js')
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // requirements for product tags
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id'
+      }
+    } 
   },
   {
     sequelize,
